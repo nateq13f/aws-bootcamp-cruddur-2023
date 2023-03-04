4673b3fb6be92a64214d51bdcc15044fef41bc7b
Nathan Wright <nqwright9311@gmail.com>
2023-03-04 03:56:54 +0000

    fixed app.py for home and notification typo

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -85,7 +85,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = NotificationActivities.run()
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
