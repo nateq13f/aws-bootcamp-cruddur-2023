3a03805833b366a011d5c2ad8c1cf2e72c0afd77
Nathan Wright <nqwright9311@gmail.com>
2023-03-27 03:56:11 +0000

    progress on week 5 again

--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -2,24 +2,25 @@ from datetime import datetime, timedelta, timezone
 
 from lib.ddb import Ddb
 from lib.db import db
+#from lib.momento import MomentoCounter
 
 class MessageGroups:
-  def run(cognito_user_id):
+  def run(user_handle):
     model = {
       'errors': None,
       'data': None
     }
 
-    sql = db.template('users','uuid_from_cognito_user_id')
-    my_user_uuid = db.query_value(sql,{
-      'cognito_user_id': cognito_user_id
-    })
+    sql = db.template('users','uuid_from_handle')
+    my_user_uuid = db.query_value(sql,{'handle': user_handle})
 
     print(f"UUID: {my_user_uuid}")
 
     ddb = Ddb.client()
     data = Ddb.list_message_groups(ddb, my_user_uuid)
-    print("list_message_groups:",data)
+    print("list_message_groups")
+    print(data)
 
+    #MomentoCounter.reset(f"msgs/{user_handle}")
     model['data'] = data
     return model
--- frontend-react-js/src/pages/MessageGroupNewPage.js
+++ frontend-react-js/src/pages/MessageGroupNewPage.js
@@ -8,9 +8,8 @@ import MessagesFeed from '../components/MessageFeed';
 import MessagesForm from '../components/MessageForm';
 import checkAuth from '../lib/CheckAuth';
 
-// [TODO] Authenication
-
 export default function MessageGroupPage() {
+  const [otherUser, setOtherUser] = React.useState([]);
   const [messageGroups, setMessageGroups] = React.useState([]);
   const [messages, setMessages] = React.useState([]);
   const [popped, setPopped] = React.useState([]);
@@ -18,18 +17,16 @@ export default function MessageGroupPage() {
   const dataFetchedRef = React.useRef(false);
   const params = useParams();
 
-  const loadMessageGroupsData = async () => {
+  const loadUserShortData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/users/@${params.handle}/short`
       const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
-      },
         method: "GET"
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setMessageGroups(resJson)
+        console.log('other user:',resJson)
+        setOtherUser(resJson)
       } else {
         console.log(res)
       }
@@ -38,9 +35,9 @@ export default function MessageGroupPage() {
     }
   };  
 
-  const loadMessageGroupData = async () => {
+  const loadMessageGroupsData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
       const res = await fetch(backend_url, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`
@@ -49,7 +46,7 @@ export default function MessageGroupPage() {
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setMessages(resJson)
+        setMessageGroups(resJson)
       } else {
         console.log(res)
       }
@@ -64,14 +61,14 @@ export default function MessageGroupPage() {
     dataFetchedRef.current = true;
 
     loadMessageGroupsData();
-    loadMessageGroupData();
+    loadUserShortData();
     checkAuth(setUser);
   }, [])
   return (
     <article>
       <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
       <section className='message_groups'>
-        <MessageGroupFeed message_groups={messageGroups} />
+        <MessageGroupFeed otherUser={otherUser} message_groups={messageGroups} />
       </section>
       <div className='content messages'>
         <MessagesFeed messages={messages} />
